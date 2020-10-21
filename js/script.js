d05987732fe288c4faca6b846151e3b29bc67148
Caterina <danizio@outlook.it>
2020-10-21 17:22:45 +0200

    Print passegger's name

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-
 // Assegnazione valore input
 
 var nome = document.getElementById('nome');
 
 console.log(nome.value);
 
-var km = document.getElementById('chilometri');
+var chilometri = document.getElementById('chilometri');
 
 console.log(chilometri.value);
 
@@ -20,7 +19,15 @@ var bottone = document.getElementById('mybutton');
 
 bottone.addEventListener('click',
  function() {
-   console.log(nome.value);
+   document.getElementById('passeggero').innerHTML = nome.value;
    console.log(chilometri.value);
    console.log(fascia.value);
+} );
+
+// Prezzo biglietto intero
+var prezzoTreno = (chilometri.value * 0.21).toFixed(2);
+
+bottone.addEventListener('click',
+ function() {
+   console.log("Costo del treno è", prezzoTreno + " euro");
 } );
