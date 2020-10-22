e149416f8fadcd5a871d045fef46efa77c0156d7
Caterina <danizio@outlook.it>
2020-10-22 23:20:02 +0200

    Print code and carriage, function's reset and genera

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+// Nascondere dettagli biglietto prima che venga premuto il bottone
+document.getElementById("biglietto").style.display = "none";
+
 // Assegnazione valore input
 
   // Nome
 
   var nome = document.getElementById('nome');
 
-  console.log(nome.value);
+  console.log('prima del click', nome.value);
 
 
 // Bottoni
@@ -16,33 +19,63 @@
 
 genera.addEventListener('click',
  function() {
+   console.log('dopo il click', nome.value);
    var km = document.getElementById('km');
 
-   var eta = document.getElementById('eta');
+   var eta = document.getElementById('eta').value;
 
    // Prezzo BIGLIETTO
+   document.getElementById("biglietto").style.display = "block";
 
    var prezzoTreno = (km.value * 0.21).toFixed(2);
    console.log(prezzoTreno);
 
    var offerta;
+   var carrozza;
+   var codice;
 
    // Prezzi scontati e offerte
-
    if ( eta == "minorenne" ) {
-           prezzoTreno = (prezzoTreno - (prezzoTreno * 0.20)).toFixed(2);
-           var offerta = "Offerta Young";
-         }
+     prezzoTreno = (prezzoTreno - (prezzoTreno * 0.20)).toFixed(2);
+     offerta = "Offerta Young";
+   }
 
-         else if ( eta == "over65" ) {
-           prezzoTreno = (prezzoTreno - (prezzoTreno * 0.20)).toFixed(2);
-           var offerta = "Offerta Silver";
-         }
-  } );
+   else if ( eta == "over65" ) {
+     prezzoTreno = (prezzoTreno - (prezzoTreno * 0.40)).toFixed(2);
+     offerta = "Offerta Silver";
+   }
 
+   else { prezzoTreno
+     offerta = "Tariffa Standard";
 
-// Output finali da stampare in pagina
+   }
 
-document.getElementById('passeggero').innerHTML = nome.value;
-document.getElementById('offer').innerHTML = offerta;
-document.getElementById('price').innerHTML = prezzoTreno;
+   var carrozza = Math.floor(Math.random() * 6) + 1;
+   console.log("Carrozza ", carrozza);
+
+    var codice = Math.floor(Math.random() * 100.000) + 90.000;
+    console.log("Codice ", codice);
+
+   // Output finali da stampare in pagina
+   document.getElementById('passeggero').innerHTML = nome.value;
+   document.getElementById('offer').innerHTML = offerta;
+   document.getElementById('carrozza').innerHTML = carrozza;
+   document.getElementById('codice').innerHTML = codice;
+   document.getElementById('price').innerHTML = prezzoTreno;
+  });
+
+  // Annullare il biglietto
+  reset.addEventListener('click',
+      function() {
+      document.getElementById('nome').value = '';
+      document.getElementById('km').value = '';
+      document.getElementById('eta').value = ('');
+      document.getElementById('passeggero').innerHTML = '';
+      document.getElementById('offer').innerHTML = '';
+      document.getElementById('carrozza').innerHTML = '';
+      document.getElementById('codice').innerHTML = '';
+      document.getElementById('price').innerHTML = '';
+      document.getElementById("biglietto").style.display = "none";
+
+      }
+    );
